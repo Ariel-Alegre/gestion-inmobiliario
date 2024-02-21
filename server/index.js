0338d60b4577eedb46a0ec70c3c9e6f99602c459
Ariel-Alegre <arielalegre98@gmail.com>
2024-02-21 12:11:44 -0300

    first commit

---
+++ server/index.js
@@ -0,0 +1,14 @@
+const server = require('./src/app.js');
+const { conn } = require('./src/db.js');
+const PORT = process.env.PORT || 3001;
+
+
+
+
+ 
+/* asdasd */
+ conn.sync({ force: false }).then(() => {
+  server.listen(PORT, () => {
+    console.log(`Servidor corriendo en el puerto ${PORT}`);
+  });
+}); 
