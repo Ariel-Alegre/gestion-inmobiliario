d96d53f17a0e13d9308fcd770b6256d51b3c3661
Ariel-Alegre <arielalegre98@gmail.com>
2024-02-21 12:41:55 -0300

    sercer

--- server/index.js
+++ server/index.js
@@ -6,7 +6,6 @@ const PORT = process.env.PORT || 3001;
 
 
  
-/* asdasd */
  conn.sync({ force: false }).then(() => {
   server.listen(PORT, () => {
     console.log(`Servidor corriendo en el puerto ${PORT}`);
