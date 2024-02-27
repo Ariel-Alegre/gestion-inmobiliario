7650329f2b94f1d61b9041386fc78b9ab1fefa09
Ariel-Alegre <arielalegre98@gmail.com>
2024-02-27 11:12:53 -0300

    server update

--- server/src/db.js
+++ server/src/db.js
@@ -6,7 +6,7 @@ const {
   DB_USER, DB_PASSWORD, DB_HOST, DB_DATABASE, DB_PORT
 } = process.env;
 
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_DATABASE}`, {
+const sequelize = new Sequelize(`postgres://postgres.dgxxbzsmvzzjarsdpukl:Programador98.@aws-0-us-west-1.pooler.supabase.com:5432/postgres`, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
   host: process.env.DB_HOST,
